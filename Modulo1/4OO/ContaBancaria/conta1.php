a77fa411303641674e00c94c176824174f3a30c0
Majo <maria.correia.s@gmail.com>
2019-09-16 00:21:23 -0300

    Tela da conta1 -  Adicionado Exibir Saldo e Formulario de Deposito

--- Modulo1/4OO/ContaBancaria/conta1.php
+++ Modulo1/4OO/ContaBancaria/conta1.php
@@ -22,7 +22,6 @@
         setcookie("numeroConta1", $conta1->numero);
         setcookie("nomeClienteConta1", $conta1->cliente);
         setcookie("saldoConta1", $conta1->getSaldo());
-
     } else {
         //Se a conta já existe cria uma nova conta a partir do cookie
         $conta1 = new Conta($_COOKIE["numeroConta1"], $_COOKIE["nomeClienteConta1"], $_COOKIE["saldoConta1"]);
@@ -30,16 +29,69 @@
 
     ?>
     <div class="container">
+        <h1>Conta 1</h1>
         <div class="row">
             <div class="col-sm-6">
-                <h1>Numero da Conta: <?= $conta1->numero ?></h1>
+                <form action="conta1.php" method="get">
+                    <input type="hidden" name="saldo">
+                    <button type="submit" class="btn btn-primary form-control">Exibir Saldo</button>
+                </form>
             </div>
             <div class="col-sm-6">
-                <h1> Cliente: <?= $conta1->cliente ?></h1>
+                <h2>
+
+                    Saldo:
+                    <?php
+                    if (isset($_GET["saldo"])) {
+                        $conta1->exibirSaldo();
+                    }
+                    ?>
+                </h2>
             </div>
         </div>
-        
+        <div class="row">
+            <div class="col-sm-6">
+                <h2>Numero da Conta: <?= $conta1->numero ?></h2>
+            </div>
+            <div class="col-sm-6">
+                <h2> Cliente: <?= $conta1->cliente ?></h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <form action="conta1.php" method="get">
+                    <fieldset>
+                        <legend>Depósito Conta</legend>
+
+                        <div class="form-group">
+                            <label>Valor: </label>
+                            <input type="number" name="depValor" class="form-control">
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary form-control">Depositar</button>
+                        </div>
+                    </fieldset>
+                </form>
+
+            </div>
+        </div>
+
     </div>
 </body>
 
 </html>
+
+<?php
+
+
+if (isset($_GET["depValor1"])) {
+    if (!empty($_GET["depValor1"])) {
+        $conta1->depositar($_GET["depValor1"]);
+        setcookie("saldoConta1", $conta1->getSaldo());
+    }
+}
+
+
+
+?>
