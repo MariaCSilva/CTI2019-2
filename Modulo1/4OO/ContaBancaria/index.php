85e6a53441c53d4e45bfeedb1d235a70e88131ac
Majo <maria.correia.s@gmail.com>
2019-09-15 23:57:29 -0300

    Adição da página inicial

---
+++ Modulo1/4OO/ContaBancaria/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Atividade Conta Bancária</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <style>
+        .my-card{
+            height: 90vh;
+            width: 90vw;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="card my-card align-items-center d-flex">
+        <div class="d-flex ml-auto mr-auto">
+            <div class="flex-fill">
+
+                <a href="conta1.php" class="btn btn-primary">Conta 1</a>
+                
+            </div>
+            
+            <div class="flex-fill">
+                
+                <a href="conta2.php" class="btn btn-primary ">Conta 2</a>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
